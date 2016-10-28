995540b8e7881ff7f41f23c298c4217129e505d6
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-10-28 15:56:30 +0300

    begin to split composition
    move common services to separate compose file
    (needs some more thinking about dependencies)

--- compose.sh
+++ compose.sh
@@ -9,118 +9,135 @@ then
 fi
 
 eval $(cat docker.priv.env)
-cmdprefix="eval $(cat docker.priv.env)"
+if [ $SUDO == true ]
+then
+    cmdenv="eval $(cat docker.priv.env) sudo -E"
+else
+    cmdenv="eval $(cat docker.priv.env) "
+fi
+cmdprefix="$cmdenv docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml"
 
 if [[ -z $ENV || ! ${environments[*]} =~ $ENV ]]
 then
   echo "ENV variable not set or has unacceptable value."
   exit 1
-fi 
+fi
 
+################ ARGUMENTS ###########
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-while getopts ":h" opt; do
-  case ${opt} in
-    h )
-      echo "Usage:"
-      echo "    ./compose.sh -h			Display this help message."
-      echo "    ./compose.sh build service		Builds a service defined in the configuration"
-      echo "    ./compose.sh up service		Runs a service as a daemon (the -d is automatically added)"
-      echo "    ./compose.sh stop service		Stops specified service"      
-      echo "    ./compose.sh openvpn		Runs the openvpn service as a daemon"
-      echo "    ./compose.sh backup			Performs a backup of the volumes listed in docker-compose.admin.yml"
-      echo "    ./compose.sh exec-bash service  Opens a bash shell inside a running container"
-      exit 0
-      ;;
-    \? )
-      echo "Invalid Option: -$OPTARG" 1>&2
-      exit 1
-      ;;
-  esac
+case $key in
+    up)
+	ACTION="up"
+	shift # past argument
+	;;
+    build)
+	ACTION="build"
+	shift # past argument
+	;;
+    stop)
+	ACTION="stop"
+	shift # past argument
+	;;
+    bash)
+	ACTION="bash"
+	shift # past argument
+	;;
+    install)
+	ACTION="install"
+	shift
+	;;
+    update)
+	ACTION="update"
+	shift
+	;;
+    --dbreset)
+	SUBACTION="--dbreset"
+	shift
+	;;
+    backup)
+        ACTION="backup"
+        shift
+        ;;
+    sslrenew)
+        ACTION="sslrenew"
+        shift
+        ;;
+    openvpn)
+        ACTION="openvpn"
+        shift
+        ;;
+    *)
+        if [ ! -z $2 ]
+        then
+            echo "Error : unknown argument"
+	    echo "Usage:      ./compose.sh [OPTION] [SUBOPTION]  service"
+            echo "* options:"
+	    echo "    up        Runs a service as a daemon (the -d is automatically added)"
+	    echo "    stop      Stops specified service"
+	    echo "    bash      Opens a bash shell inside a running container"
+	    echo "    install   Runs install procedure and exits"
+	    echo "    update    Runs update procedure and exits"
+            echo "* suboptions (only for install & update):"
+            echo "    --dbreset resets the database"
+            echo "    backup	Performs a backup of the volumes listed in docker-compose.admin.yml"
+	    exit 1
+        fi
+	echo "service to manage : $1"
+	service=$1
+	shift
+
+	;;
+esac
 done
-shift $((OPTIND -1))
 
 
-subcommand=$1; shift  # Remove 'compose.sh' from the argument list
-case "$subcommand" in
-  # Parse options to the install sub command
-  pull)
-    service=$1; shift  # Remove 'build' from the argument list
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml pull $service"  
-    ;;
-    
-  build)
-    service=$1; shift  # Remove 'build' from the argument list
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml build --pull $service"
-    ;;
-    
-  up)
-    # Process package options
-    while getopts ":d:" opt; do
-      case ${opt} in
-        d )
-          service=$OPTARG
-          ;;
-        \? )
-          echo "Invalid Option: -$OPTARG" 1>&2
-          exit 1
-          ;;
-        : )
-          echo "Invalid Option: -$OPTARG requires an argument" 1>&2
-          exit 1
-          ;;
-      esac
-    done
-    if [ -z $service ]
-    then
-      if [ -z $1 ]
-      then
-        echo "Missing option : up requires an argument (the service)"
-        exit 1
-      else
-        service=$1; shift
-      fi
-    fi      
-    shift $((OPTIND -1))
+case "$ACTION" in
+    # Parse options to the install sub command
+    build)
+	command="$cmdprefix build --pull $service"
+	;;
 
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml up -d $service"
-    echo $command    
-    ;;
-    
-  stop)
-    if [ ! -z $1 ]
-    then
-      service=$1; shift
-    fi
-    shift $((OPTIND -1))
+    install)
+	command="$cmdprefix run --rm $service --install"
+	;;
 
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml stop $service"
-    echo $command    
-    ;;    
-    
-  openvpn)  
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml -f docker-compose.admin.yml up -d openvpn"
+    update)
+	command="$cmdprefix run --rm $service --update"
+	;;
+    up)
+	command="$cmdprefix up -d $service"
+	;;
+
+    stop)
+	command="$cmdprefix stop $service"
+	;;
+
+    bash)
+	command="$cmdprefix exec $service /bin/bash"
+	;;
+
+    backup)
+        echo "Performing backup  of docker volumes in /backup"
+        command="$cmdprefix -f docker-compose.admin.yml run --rm backup"
+        ;;
+    sslrenew)
+        command="$cmdprefix -f docker-compose.admin.yml run --rm letsencrypt renew"
+        ;;
+
+    openvpn)
+        command="$cmdprefix -f docker-compose.admin.yml up -d openvpn"
     ;;
-    
-  backup)
-    echo "Performing backup  of docker volumes in /backup" 
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml -f docker-compose.admin.yml run --rm backup"
-    ;;
-    
-  exec-bash)
-    if [ -z $1 ]
-    then
-      echo "Missing option : exec-bash requires an argument (the service)"
-      exit 1
-    else
-      service=$1; shift
-      command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml exec $service /bin/bash"
-      echo $command
-    fi  
-    ;;
-    
-    sslrenew)  
-    command="$cmdprefix sudo -E docker-compose -f ./docker-compose.yml -f docker-compose.$ENV.yml -f docker-compose.admin.yml run --rm letsencrypt renew"
-    ;;
+
 esac
 
+if [ ! -z "$SUBACTION" ]
+then
+    echo "subaction given $SUBACTION"
+    command="$command $SUBACTION"
+fi
+
+echo $command
 $command
